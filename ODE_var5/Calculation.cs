8017edd3e1d142a8d16feda91529e127d14cc3b4
Daria <61648155+Tsvetkova13@users.noreply.github.com>
2022-03-19 10:29:08 +0300

    Add first commit

---
+++ ODE_var5/Calculation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.OdeSolvers;
+
+namespace ODE_var5
+{
+    public class Calculation
+    {
+
+    }
+}
---
+++ ODE_var5/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.OdeSolvers;
+
+namespace ODE_var5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CalcRK4()
+        {
+            Vector<double> y0 = Vector<double>.Build.Dense(2);
+            y0[0] = 2.0;
+            y0[1] = 0.0;
+
+            double t0 = 0;
+            double tmax = 0.1;
+            double tau = 0.01;
+            int N = Convert.ToInt32((tmax - t0) / tau);
+
+            //Vector<double> res = Vector<double>.Build.Dense(2);
+
+            Func<double, Vector<double>, Vector<double>> f = (t, y) => Vector<double>.Build.Dense(new[] { y[1], ((1 - y[0] * y[0]) * y[1] - y[0]) });
+
+            Vector<double>[] res = RungeKutta.FourthOrder(y0, t0, tmax, N, f);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalcRK4();
+            
+        }
+    }
+}
